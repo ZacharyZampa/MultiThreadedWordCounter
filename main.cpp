cb463659f7c6781eed9f5885167010fbf928d57f
Zachary Zampa <zackzampa@gmail.com>
2019-11-08 20:43:08 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,25 +14,41 @@
 #include <cctype>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <functional>
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
 #include <sstream>
+#include <chrono> 
 
 using namespace std;
+using namespace std::chrono;
 
 using StrMap = std::unordered_map<std::string, bool>;
 using StrVec = std::vector<std::string>;
+using StrQueue = std::queue<std::string>;
+
 
 const std::string dictionaryFile = "english.txt";
-const std::string instruct = 
-    "Please entire the desired file path or type exit to quit";
+const std::string instruct =
+        "Please entire the desired file path or type exit to quit";
 StrMap dictMap;
+StrQueue jobs;  // hold the lines to be worked on by the programs
+const size_t maxQSize = 100;
 
-std::mutex mut;
+std::condition_variable queueBlocker;  // condition variable -- avoid spin locks
+std::mutex queueMutex;  // mutex for synchronized operations on the queue
+
+// total count of words and valid count
+std::atomic<size_t> totalCount = ATOMIC_VAR_INIT(0);
+std::atomic<size_t> validCount = ATOMIC_VAR_INIT(0);
+std::atomic<bool> isReading = ATOMIC_VAR_INIT(true);  // bool true if still read
 
 /**
  * Load the dictionary
@@ -57,7 +73,6 @@ StrMap dictionary(const std::string& file = dictionaryFile) {
  */
 bool isValidWord(const StrMap& dict, const std::string& word) {
     auto it = dict.find(word);
-//    std::cout << dict.size();
 
     if (it == dict.end()) {
         // not valid
@@ -97,22 +112,17 @@ std::string toLowerCase(const std::string& str) {
  * @param line
  * @param counts
  */
-void updateWordCounts(const StrMap& dict, std::string& line,
-        int counts[]) {
+void updateWordCounts(std::string line) {
     line = cleanLine(line);
     std::istringstream ss(line);
     std::string word;
     // loop through each word and update counters
     while (ss >> word) {
-        mut.lock();  // lock for increasing count operation
-        counts[0]++;  // update total word count
-        mut.unlock();  // done with increasing count operation
+        totalCount++;  // update total word count
         std::string lowered = toLowerCase(word);
-        if (isValidWord(dict, lowered)) {
+        if (isValidWord(dictMap, lowered)) {
             // this is a valid word
-            mut.lock();  // lock for increasing count operation
-            counts[1]++;  // increase valid word counts
-            mut.unlock();  // done with increasing count operation
+            validCount++;  // increase valid word counts
         }
     }
 }
@@ -122,26 +132,68 @@ void updateWordCounts(const StrMap& dict, std::string& line,
  * @param counts
  * @return string of info
  */
-std::string getStats(int counts[]) {
-    return "Words: " + to_string(counts[0])
-            + ", Valid Words: " + to_string(counts[1]);
+std::string getStats() {
+    return "Words: " + to_string(totalCount)
+            + ", Valid Words: " + to_string(validCount);
 }
 
+/**
+ * Read the file in and add each line to the job queue for the producer
+ * @param fp The file path
+ */
+void reader(std::string& fp) {
+    ifstream filest(fp);
+    std::string line;
+
+    // read each line and add to queue
+    while (std::getline(filest, line)) {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        queueBlocker.wait(lock, [] { return jobs.size() < maxQSize; });
+        {
+            jobs.push(line);  // add to end of queue
+            // Wake-up a thread that is waiting for entry to be added
+            queueBlocker.notify_one();
+        }
+    }
+
+    isReading = !isReading;  // done reading
+}
 
 /**
- * Run the specified thread
- * @param list
- * @param result
- * @param startIdx
- * @param count
+ * Thread-safe Check if the job queue is empty
+ * @return 
  */
-void thrMain(const StrVec fullFile, int counts[], 
-        const int startIdx, const int count) {
-    int end = startIdx + count;
-    for (int i = startIdx; i < end; i++) {
-        std::string line = fullFile[i];
-//        std::cout << dictMap.size();
-        updateWordCounts(dictMap, line, counts);
+bool jobEmptyLoopChecker() {
+    bool flag;
+    queueMutex.lock();
+    flag = jobs.empty();
+    queueMutex.unlock();
+    return flag;
+}
+
+/**
+ * Count all words in a file
+ * Functions a the consumer in the producer-consumer threading model
+ * This consumer reads from the queue all of the produced input
+ */
+void counter() {
+    // loop while producer is working or queue is not empty
+    while (isReading || !jobEmptyLoopChecker()) {
+        std::string line;  // declare variable before lock
+        {
+            // add the lock
+            std::unique_lock<std::mutex> lock(queueMutex);
+            queueBlocker.wait(lock, [] {
+                return !jobs.empty(); });
+            line = jobs.front();
+            jobs.pop();
+
+            // unlock
+            queueBlocker.notify_one();
+        }
+
+        // process the data
+        updateWordCounts(line);
     }
 }
 
@@ -154,35 +206,20 @@ void thrMain(const StrVec fullFile, int counts[],
  */
 void threadLauncher(std::vector<std::thread>& threadGroup, int cores,
         std::string& fp) {
-    // figure out how to divide up
-    int size;
-    
-    ifstream filest(fp);
-    StrVec fullFile;
-    std::string line;
-    while (std::getline(filest, line)) {
-        fullFile.push_back(line);
-    }
-    
-    int ffs = fullFile.size();
-    size = ffs / cores;
-    
-    int counts[2] = {0, 0};  // hold word counts: 0 = total, 1 = valid
+    // spawn a producer thread that reads in the files
+    threadGroup.push_back(std::thread(reader, std::ref(fp)));
 
-    // begin making threads
-    for (int thr = 0, start = 0; thr < cores; thr++, start += size) {
-        if (cores != 1 && thr + 1 == cores && ffs == thr)
-            size++;
-        threadGroup.push_back(std::thread(thrMain, 
-                std::ref(fullFile), std::ref(counts), start, size));
+    // begin spawning threads -- using up the remaining threads
+    for (int thr = 1; thr < cores; thr++) {
+        threadGroup.push_back(std::thread(counter));
     }
-    
+
     // join all threads
     for (auto& t : threadGroup) {
         t.join();
     }
-    
-    std::cout << getStats(counts);
+
+    std::cout << getStats() << std::endl;
 }
 
 /*
@@ -190,27 +227,43 @@ void threadLauncher(std::vector<std::thread>& threadGroup, int cores,
  */
 int main(int argc, char** argv) {
     // Load the dictionary
-    dictMap = dictionary(dictionaryFile);
+    dictionary(dictionaryFile);
 
     // determine how many cores can be used
-    size_t cores = std::thread::hardware_concurrency() / 2;  // only physical
+    size_t cores = std::thread::hardware_concurrency();
     std::cout << cores << " concurrent threads are supported.\n";
-    
+
     std::string resp;
     std::cout << instruct << std::endl;
     while (std::cin >> resp) {
         if (resp == "exit" || resp == "quit") {
             return 0;
         }
-
-        // get file path
-        std::string fp = resp;
         
+        // Get starting timepoint -- for timing
+        auto start = high_resolution_clock::now();
+    
         // launch threads to process
         std::vector<std::thread> threadGroup;
-        threadLauncher(threadGroup, cores, fp);
+        threadLauncher(threadGroup, cores, resp);
         
+        // Get ending timepoint -- for timing
+        auto stop = high_resolution_clock::now(); 
+        
+        // Get duration. Substart timepoints to  
+        // get durarion. To cast it to proper unit 
+        // use duration cast method 
+        auto duration = duration_cast<microseconds>(stop - start); 
+  
+        cout << "Time taken by function: "
+            << duration.count() << " microseconds" << endl; 
+
         std::cout << std::endl << instruct << std::endl;  // prompt for next
+        
+        // Reset global variables for next process
+        isReading = true;  // set as producer as reading again for next process
+        totalCount = 0;
+        validCount = 0;
     }
 
     return 0;
